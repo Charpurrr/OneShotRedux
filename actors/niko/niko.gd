f6e202c4fe9dd79857bb9657645e7ad5c28354bb
dignityloss <60553204+dignityloss@users.noreply.github.com>
2023-04-09 17:37:10 +0200

    niko movement

---
+++ actors/niko/niko.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+var speed = 100
+
+func _physics_process(delta):
+	
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
+	input_vector.y = Input.get_action_strength("Down") - Input.get_action_strength("Up")
+
+	if input_vector != Vector2.ZERO:
+		velocity = input_vector * speed
+	else:
+		velocity = Vector2.ZERO
+	move_and_slide()
