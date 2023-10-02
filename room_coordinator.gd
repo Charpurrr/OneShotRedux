4a7cd63ca2cf60ef52ca8708130a76b05e7cd4b1
Charpurrr <yyoshidevv@gmail.com>
2023-10-02 23:36:07 +0200

    error fix

--- room_coordinator.gd
+++ room_coordinator.gd
@@ -4,16 +4,15 @@ extends Node2D
 
 @onready var wm : Node2D = owner
 
-var current_room : Node2D
+var current_room : Node2D = null
 
 
 ## Transitions and updates to a new room
 func upd_current_room(new_area : StringName, new_room : StringName):
 	var instantiated_room : Node2D = wm.areas[new_area].rooms[new_room].instantiate()
-	var previous_room = get_child(0)
 
-	if previous_room != null:
-		previous_room.queue_free()
+	if current_room != null:
+		current_room.queue_free()
 
 	add_child(instantiated_room)
 
