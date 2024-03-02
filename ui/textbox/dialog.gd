33c2e44b317f85175b7eb83da0f366a98ecc79ca
CharlotteaDev <66517745+Charpurrr@users.noreply.github.com>
2024-03-02 02:25:29 +0100

    Interactable objects (WIP 2)
    progression and skipping

--- ui/textbox/dialog.gd
+++ ui/textbox/dialog.gd
@@ -2,7 +2,10 @@ class_name Dialog
 extends CanvasLayer
 ## Character dialogue.
 
+
+@onready var prog_graphic: AnimatedSprite2D = $Progress
 @onready var text_label: Label = $Text
+
 var sfx: AudioStreamWAV = preload("res://sound/sfx/text_tick.wav")
 
 ## Timer for text speed.
@@ -11,6 +14,8 @@ var read_timer: int = 0
 ## Time between the ticking sound effects.
 const SFX_TIME: int = 5
 var sfx_timer: int = 0
+
+var can_progress: bool = false
 
 #region INPUT VARIABLES, THESE ARE SET BY THE DIALOG CALLER.
 var text: String
@@ -25,18 +30,51 @@ func _ready():
 
 
 func _process(_delta):
-	read_timer = max(read_timer - 1, 0)
-	sfx_timer = max(sfx_timer - 1, 0)
+	_text_progress()
 
-	if not text_label.visible_characters < text.length():
+	if can_progress:
 		return
 
-	if read_timer == 0:
+	if Input.is_action_just_pressed(&"interact"):
+		text_label.visible_characters = text.length()
 
+	_characters_appear()
+	_ticking_sfx()
+
+
+## Handles appearing character logic.
+func _characters_appear():
+	read_timer = max(read_timer - 1, 0)
+
+	if read_timer == 0:
 		text_label.visible_characters += 1
 		read_timer = speed
+
+
+## Handles the text tick sound effect.
+func _ticking_sfx():
+	sfx_timer = max(sfx_timer - 1, 0)
 
 	if sfx_timer == 0:
 		SFX.play_sfx(sfx, self, &"SFX")
 
 		sfx_timer = SFX_TIME
+
+
+## Handles dialogue progression.[br]
+## [i] NEW DIALOG PROMPT CALLED IN TEXT EDIT WITH: « \new »[br][br]
+## EXAMPLE: « "Hello! \new Hi!" »[br]
+## OUTPUT: Dialog prompt 1: "Hello!", dialog prompt 2: "Hi!"
+func _text_progress():
+	can_progress = text_label.visible_characters == text.length()
+	prog_graphic.visible = can_progress
+
+	if not can_progress:
+		return
+
+	if Input.is_action_just_pressed(&"interact"):
+		#prog_graphic.play(&"pressed")
+		text_label.visible_characters = 0
+	#elif Input.is_action_just_released(&"interact"):
+	#else:
+		#prog_graphic.play(&"unpressed")
